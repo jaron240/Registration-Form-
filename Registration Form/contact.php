d05c879cc5ed5d70c617a5ebdee54e79028a5a34
jaron240 <jaron.luedemann09@gmail.com>
2024-12-16 21:28:51 +0100

    6. Footer weiter gebaut +Impressum

---
+++ Registration Form/contact.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+</head>
+<body>
+    
+<form action="contact.php" method="post">
+    <input type="text" name ="name" placeholder="name"> <br>
+    <button type="submit" name = "submit">Submit</button>
+
+
+
+</form>
+</body>
+</html>
